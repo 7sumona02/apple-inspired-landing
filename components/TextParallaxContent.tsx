f8a35a8f228326e955a792506dc1767750bb1f7e
7sumona02 <sumona.23548@sscbs.du.ac.in>
2024-12-18 21:54:55 +0530

    Initial commit from Create Next App

---
+++ components/TextParallaxContent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TextParallaxContentExample = () => {
+  return (
+    <div>TextParallaxContent</div>
+  )
+}
+
+export default TextParallaxContentExample
+
+const TextParallaxContent = ({img})
